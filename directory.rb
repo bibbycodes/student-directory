e2ef876a772f07a5d9058fb28912629b3321a9e7
Robert Rosiji-Griffith <bibbycodes@gmail.com>
2019-09-30 16:44:10 +0100

    adds load and save functionality

--- directory.rb
+++ directory.rb
@@ -39,6 +39,8 @@ end
 def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit"
 end
 
@@ -54,11 +56,34 @@ def process(selection)
       @students = input_students
     when "2"
       show_students
+    when "3"
+      save_students
+    when "4"
+      load_Students
     when "9"
       exit
     else 
       puts "Please enter a valid number"
   end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def load_Students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
 end
 
 def interactive_menu
